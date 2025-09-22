0934da82a40e11f02e7cb9d54a06d3e19dba7b3d
gvkumarg <gvkumarg@gmail.com>
2025-09-22 18:14:28 +0530

    conversation added

---
+++ 02-conversation.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Conversation script using variables
+# Print conversation
+echo "Ramesh: Hello Suresh, how are you?"
+echo "Suresh: I'm fine too. Are you coming to the office tomorrow?"
+echo "Ramesh: Yes, I will be there. Let's meet at lunch."
+echo "Suresh: Sure, see you then!"
+echo "Ramesh: Welcome"
