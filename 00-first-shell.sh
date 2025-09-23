ae8ed12fbea17d8a553a0120515a14fbe9a086ea
gvkumarg <gvkumarg@gmail.com>
2025-09-23 07:18:17 +0530

    read the value form input and arugs

--- 00-first-shell.sh
+++ 00-first-shell.sh
@@ -3,7 +3,11 @@
 read -p "Enter name for person3: " person3
 person1="Ramesh"
 person2="Suresh"
-echo "Welcome to Hello world"
+person4="${4:-Prakash}"   # use first arg or default
+person5="${5:-Rani}"
+echo "Welcome to world"
 echo "$person1: Hello"
 echo "$person2: Hello"
 echo "$person3: Hello"
+echo "$person4: Hello"
+echo "$person5: Hello"
