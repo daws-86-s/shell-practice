727bcca25e41b84ddb1156f6e4318a15b402a24d
gvkumarg <gvkumarg@gmail.com>
2025-09-23 07:14:36 +0530

    read the value form input

--- 00-first-shell.sh
+++ 00-first-shell.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 # comment: what the script does
-read -p "Enter name for person1: " person1
+read -p "Enter name for person3: " person3
 person1="Ramesh"
 person2="Suresh"
 echo "Welcome to Hello world"
 echo "$person1: Hello"
 echo "$person2: Hello"
+echo "$person3: Hello"
