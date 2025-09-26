b43bdde41c8a3c8606ccfd831567104d56599cc7
gvkumarg <gvkumarg@gmail.com>
2025-09-26 22:31:30 +0530

    learning devops

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,5 +1,6 @@
-#!/bin/bash
+
 #this not executed just for information purpose only
 echo "Hello-World"
 echo "Welcome to DevOps"
-echo "Welcome to shell scripting........../*******/@@@@@" 
+echo "Welcome to shell scripting........../*******/@@@@@@@@@@" 
+echo "Suresh and vijay are learning shell"
