7a922f700ba3bbbee0a634957427eeede572cee8
daws-86-s <gvkumar061982@gmail.com>
2025-09-26 22:32:47 +0530

    Update 01-hello-world.sh
    committed in repo

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,6 +1,7 @@
-
+#!/bin/bash
 #this not executed just for information purpose only
 echo "Hello-World"
 echo "Welcome to DevOps"
 echo "Welcome to shell scripting........../*******/@@@@@@@@@@" 
 echo "Suresh and vijay are learning shell"
+echo "simple shell program"
